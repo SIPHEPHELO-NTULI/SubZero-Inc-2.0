be5d6c9ab79fe1d577aa4b1dffc82d95910b4790
KGovender <1821334@students.wits.ac.za>
2022-10-12 23:48:38 +0200

    Update auth_service.dart

--- lib/Firebase/auth_service.dart
+++ lib/Firebase/auth_service.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:give_a_little_sdp/Encryption/encryption.dart';
 
 //This class is used to verify the details of an existing user
 //And Sign out a user as well as create a new user
@@ -11,14 +9,8 @@ class AuthService {
   AuthService({required this.auth});
   //sign out method using firebase Auth
   Future<String> signOut() async {
-    try {
-      await auth.signOut();
-      return "Success";
-    } on FirebaseAuthException catch (e) {
-      return e.message.toString();
-    } catch (e) {
-      rethrow;
-    }
+    await auth.signOut();
+    return "Success";
   }
 
   //sign in method using email and password using firebase Auth
@@ -33,12 +25,7 @@ class AuthService {
 
 //Used to sign up new users using firebase Auth
   Future<String> signUp(email, password) async {
-    try {
-      await auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      return "Account Created";
-    } on FirebaseAuthException catch (e) {
-      return e.message.toString();
-    }
+    await auth.createUserWithEmailAndPassword(email: email, password: password);
+    return "Account Created";
   }
 }
