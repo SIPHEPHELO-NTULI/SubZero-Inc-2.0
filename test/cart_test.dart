c298cbec423430c747380c62e3d3893dea8205d1
KGovender <1821334@students.wits.ac.za>
2022-10-24 23:53:31 +0200

    unit tests for wishlist

--- test/cart_test.dart
+++ test/cart_test.dart
@@ -12,13 +12,4 @@ void main() {
     final cartTotal = CartTotal();
     expect(cartTotal.getCartTotal(items), "150");
   });
-
-  test('Cart total returns 150 when given 100 and 50', () {
-    List items = [
-      {'price': '100'},
-      {'price': '50'}
-    ];
-    final cartTotal = CartTotal();
-    expect(cartTotal.getCartTotal(items), "150");
-  });
 }
